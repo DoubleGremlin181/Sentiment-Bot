27e14ff871f21c71ca620982bd0c484db02c1c1d
DoubleGremlin181 <khukmani@gmail.com>
2018-02-21 23:53:50 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python botMain.py
---
+++ botMain.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import asyncio
+from textblob import TextBlob
+
+Client = discord.Client()
+bot = commands.Bot(command_prefix = "s.")
+
+@bot.event
+async def on_ready():
+    print ("Bot is online")
+    print ("Username: ",bot.user.name)
+    print ("*********************")
+
+@bot.command()
+async def text(*arg):
+    blob = TextBlob(" ".join(arg))
+    sent = blob.sentiment.polarity
+    sub = blob.sentiment.subjectivity
+    if(sent < -0.5):
+        await  bot.say("The statement is negative")
+    elif(sent < -0.1):
+        await  bot.say("The statement is slightly negative")
+    elif(sent > -0.1 and sent < 0.1):
+        await  bot.say("The statement neutral")
+    elif(sent< 0.5):
+        await  bot.say("The statement is slightly positive")
+    elif(sent <= 1):
+        await  bot.say("The statement is positive")
+    if(sub > 0.8):
+        await  bot.say("It is subjective")
+    elif(sub > 0.4):
+        await  bot.say("It maybe subjective")
+
+
+
+bot.run("NDE1OTEwNzMyOTA4MDY4ODc0.DW8zrA.PEDhp_3Cz5DDOBWx-NIThWVsSjQ")
